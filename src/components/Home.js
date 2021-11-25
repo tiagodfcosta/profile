2bdc4382db4246127baac6ec199fb20994ff2d31
tiagodfcosta <83545729+tiagodfcosta@users.noreply.github.com>
2021-11-25 13:24:17 +0000

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
 import './Home.css';
+
+
 function Home() {
     return (
      <div className="home">
          <h2>Hello, I'm a full-stack developer based in Portugal</h2>
-         <p>teste</p>
+         <div className="container"> 
+            <div className="containerDescription"> 
+                <h1>Tiago Costa</h1>
+                <p>JavaScript Full Stack Developer</p>
+            </div>
+            <div className="containerImg" >
+                <img src="./tiago_costa.jpeg" alt="Tiago Costa"  height="150px" />
+            </div>
+         
+         </div>
      </div>
     )
 }

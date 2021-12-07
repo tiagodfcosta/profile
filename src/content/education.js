f68ee96c468b8dc9b5b359c2245122e8ced35b01
tiagodfcosta <83545729+tiagodfcosta@users.noreply.github.com>
2021-12-07 20:00:10 +0000

    about menu accordion

---
+++ src/content/education.js
@@ -0,0 +1,23 @@
+export const education = [
+    {
+        title: 'BOOTCAMP PROGRAMAÇÃO',
+        content: 'Front end development HTML, CSS and JavaScript +React \n Back end development MongoDB and JavaScript +NodeJS',
+        duration: '400H - 2021'
+    },
+    {
+        title: 'POWER UP',
+        content: 'INICIAÇÃO À PROGRAMAÇÃO MULTIMÉDIA - FOTOGRAFIA,VÍDEO, SOM E DESIGN',
+        duration: '400H - 2021'
+    },
+    {
+        title: 'FREQUÊNCIA UNIVERSITÁRIA - CURSO DE GEOLOGIA',
+        content: 'Faculdade de Ciências da Universidade de Lisboa',
+        duration: '2011 - 2014'
+    },
+    {
+        title: '12º ANO, CURSO CIENTÍFICO-HUMANÍSTICO DE CIÊNCIAS E TECNOLOGIAS',
+        content: 'Agrupamento de Escolas Ibn Mucana, Alcabideche',
+        duration: '2010 - 2011'
+    }
+ 
+  ];
---
+++ src/content/experience.js
@@ -0,0 +1,12 @@
+export const experience = [
+    {
+        title: 'ANALISTA DE CRÉDITO HABITAÇÃO',
+        content: 'Doutor Finanças',
+        duration: 'Mar 2020 - Mar 2021'
+    },
+    {
+        title: 'RESPONSÁVEL DE LOJAS MOBICASCAIS',
+        content: 'Cascais Próxima',
+        duration: 'Set 2014 - Mar 2020'
+    }
+  ];

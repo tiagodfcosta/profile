e5662fe8e38583803604eb3e6508c54e35971dad
tiagodfcosta <83545729+tiagodfcosta@users.noreply.github.com>
2021-12-10 01:39:26 +0000

    color scheme + debug CSS

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,12 @@
 import ProjectsCSS from './Projects.module.css';
 import React from 'react'
 
+
 function Projects() {
     return (
-      <div className={ProjectsCSS.container}>
-          <div className={ProjectsCSS.title}><h1>Projects</h1></div>
-      </div>
+     <div className={ProjectsCSS.container}>
+       <h2>2021 - Projects</h2>
+     </div>
     )
 }
 
---
+++ src/content/projects.js
@@ -0,0 +1,14 @@
+export const projects = [
+    {
+        title: 'Graconnet',
+        image:"./logo192.png",
+        content: 'Doutor Finanças',
+        duration: 'Jul 2021'
+    },
+    {
+        title: 'Profile',
+        image: "./logo192.png",
+        content: 'Cascais Próxima',
+        duration: 'Dez 2021'
+    }
+  ];

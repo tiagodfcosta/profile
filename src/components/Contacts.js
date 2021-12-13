b87af9cf4ba032962e0d43ba522788cd58ddca07
tiagodfcosta <83545729+tiagodfcosta@users.noreply.github.com>
2021-12-13 21:44:47 +0000

    css add's + projects

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,8 +3,11 @@ import React from 'react'
 
 function Contacts() {
     return (
-      <div className={ContactsCSS}>
-          <h1>Contacts</h1>
+      <div className={ContactsCSS.container}>
+          <p><strong>email:</strong> tiagodf.costa@gmail.com</p>
+          <p><a href="https://www.linkedin.com/in/tiago-costa-558054169/" target="_blank" rel="noopener noreferrer"> <strong>LinkedIn</strong></a></p>
+          <p><a href="https://github.com/tiagodfcosta" target="_blank" rel="noopener noreferrer"> <strong>GitHub</strong></a></p>
+
       </div>
     )
 }
--- src/content/education.js
+++ src/content/education.js
@@ -1,7 +1,15 @@
 export const education = [
     {
         title: 'BOOTCAMP PROGRAMAÇÃO',
-        content: `Front end development HTML, CSS, JavaScript and React || Back end development MongoDB and JavaScript +NodeJS`,
+        content: `For 10 weeks I took part in the intensive programming Boot Camp of Bytes4Future. In this program I learned to write JavaScript (ES6) code, I also learned the basis of topics such as Object Oriented Programming, Data Structures, Algorithms, Functional Programming and Basic Software Principles.
+
+        Starting from the HTML and CSS base acquired in the PowerUp stage of Bytes4Future, I learned to develop interfaces using the declarative model, more specifically, using React. I also learned what Redux is all about.
+        
+        I learned how to use JavaScript in the backend using NodeJS to run JavaScript code outside the browser. I developed REST APIs using Express and had my first chance to play with Web Sockets.
+        
+        To store application data, I learned how to use files, most of the time to store JSON objects. I also learned to work with MongoDB and learned the basics of SQL.
+        
+        During the development of projects I used Git and GitHub to manage source code versions.`,
         duration: '400H - 2021'
     },
     {

a8684567d4ec52b9c7356ca3778090bd84467bf0
tiagodfcosta <83545729+tiagodfcosta@users.noreply.github.com>
2021-12-14 02:58:26 +0000

    projects garconet

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -22,7 +22,7 @@ import React, {useState} from 'react'
     }
       
        return (
-          <>
+          <div className={ProjectsCSS.FullContainer}>
               <div className={ProjectsCSS.container}>
                 <div className={ProjectsCSS.projects}>
                   <div className={ProjectsCSS.Container1} onClick={chageStateGarconet}> 
@@ -36,12 +36,26 @@ import React, {useState} from 'react'
                 </div>
                 
               </div>
-              <div className={ProjectsCSS.teste}>
-              {garconetOn && <div className={ProjectsCSS.um}><p>teste</p></div>}  
-              {siteOn && <div className={ProjectsCSS.um}><p>teste2</p></div>}
+              <div className={ProjectsCSS.containerProjects}>
+              {garconetOn && <div className={ProjectsCSS.Garconet}>
+                  <img className={ProjectsCSS.img1} src="../garconetlogo.png" alt="Garconet" />
+                  <div className={ProjectsCSS.GNcontent}>
+                   <p>The team for this project/challenge was composed by three people. The problem that we defined was the waiting times in restaurants, when we want to order food or beverages and no one is coming to get our order. We decided to create a Web App for restaurants in which it has a QR Code Reader and it could 'read' in which table we were and we could start to ask for food and beverages with that same application. We had two front-ends, one back-end and a database (in which we had the menu and the requests for the day). The initial idea was to create everything using Next.js and to connect it with WebSockets... Unfortunately we weren't able to do it. So we opted to use React.js for front-end, Express.js for back-end and MongoDB as our database.</p> 
+                    <div className={ProjectsCSS.App}>
+                      <img className={ProjectsCSS.imgAPP} src="../garconet1.png" alt="Garconet1" />
+                      <img className={ProjectsCSS.imgAPP} src="../garconet2.png" alt="Garconet2" />
+                      <img className={ProjectsCSS.imgAPP} src="../garconet3.png" alt="Garconet3" />
+                    </div>
+
+                  </div>
+                </div>}  
+              {siteOn && <div className={ProjectsCSS.Site}>
+                  <h2>2021 - website</h2>
+                  <img className={ProjectsCSS.img2}  src="../logo192.png" alt="logo" />               
+                </div>}
               
             </div>
-          </>
+          </div>
       )
   } 
 
